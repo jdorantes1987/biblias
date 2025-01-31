de2e3d0554f6aef018a94b810e84fcba042d1696
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-01-31 08:44:39 -0400

    Se modificó la forma de buscar en cada versículo. Se combinó la tabla de libros con la tabla versiculos.

--- scripts/oBiblias.py
+++ scripts/oBiblias.py
@@ -2,7 +2,7 @@ from sqlite3 import connect
 
 from scripts.db_sqlite import BD_SQLite_Biblias as DB
 
-db_path = r"D:\Samsung/Personal/Estudios_Bbcs/Biblias Sqlite/BLPH/BLPH.SQLite3"
+db_path = r"F:\Samsung/Personal/Estudios_Bbcs/Biblias Sqlite/BLPH/BLPH.SQLite3"
 
 
 class DataBiblias:
@@ -16,6 +16,7 @@ class DataBiblias:
 if __name__ == "__main__":
     data = DataBiblias()
     df = data.get_biblia_BLPH()
-    df = df[df["text"].str.contains("atentamente", case=False)]
+    palabras = ["interior"]
+    df = df[df["text"].str.contains("|".join(palabras), regex=True)]
     df["text"] = df["text"].str[:140]
     print(df.to_string())
