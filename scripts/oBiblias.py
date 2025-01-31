e5cba80a17b4f5f18a41647425fb4fd49beae9b3
jdorantes <165170008+jdorantes1987@users.noreply.github.com>
2025-01-31 15:37:30 -0400

    Actualización

--- scripts/oBiblias.py
+++ scripts/oBiblias.py
@@ -2,10 +2,6 @@ from sqlite3 import connect
 
 from scripts.db_sqlite import BD_SQLite_Biblias as DB
 
-p_BLPH = r"F:\Samsung/Personal/Estudios_Bbcs/Biblias Sqlite/BLPH/BLPH.SQLite3"
-p_NRV1990 = r"F:\Samsung/Personal/Estudios_Bbcs/Biblias Sqlite/NRV1990/NRV1990.SQLite3"
-p_RVA = r"F:\Samsung/Personal/Estudios_Bbcs/Biblias Sqlite/RVA/RVA.SQLite3"
-
 
 class DataBiblias:
     def __init__(self, p_biblia) -> None:
@@ -16,9 +12,15 @@ class DataBiblias:
 
 
 if __name__ == "__main__":
+    p_BLPH = r"F:\Samsung/Personal/Estudios_Bbcs/Biblias Sqlite/BLPH/BLPH.SQLite3"
+    p_NRV1990 = (
+        r"F:\Samsung/Personal/Estudios_Bbcs/Biblias Sqlite/NRV1990/NRV1990.SQLite3"
+    )
+    p_RVA = r"F:\Samsung/Personal/Estudios_Bbcs/Biblias Sqlite/RVA/RVA.SQLite3"
+
     data = DataBiblias(p_biblia=p_RVA)
     df = data.get_biblia()
-    palabras = ["Abram"]
+    palabras = ["Jesucristo"]
     df = df[df["text"].str.contains("|".join(palabras), regex=True)]
     df["text"] = df["text"].str[:140]
     print(df.to_string(index=False))
